2610ba0eea827cae842c232d3d6a578b86448678
Mohamed Aiman <aiman1717a@gmail.com>
2020-06-20 20:23:15 -0700

    Update ToolServiceProvider.php

--- src/ToolServiceProvider.php
+++ src/ToolServiceProvider.php
@@ -9,7 +9,7 @@ use Aiman\MenuBuilder\Http\Resources\MenuResource;
 use Laravel\Nova\Events\ServingNova;
 use Laravel\Nova\Nova;
 
-class MenuBuilderServiceProvider extends ServiceProvider
+class ToolServiceProvider extends ServiceProvider
 {
     /**
      * Bootstrap any application services.
